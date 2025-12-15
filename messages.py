b440a178a377e7434df2a7c44493117f7883e84b
petersitt <peter@microcollege.net>
2025-12-15 19:52:55 +0700

    Create messages.py

---
+++ messages.py
@@ -0,0 +1,10 @@
+greeting="Hello, world!"
+goodbye="Bye bye, friend."
+Explosion="Boom!"
+Laser="Zap!"
+Hello="Hello, friend."
+print(greeting)
+print(goodbye)
+print(Explosion)
+print(Laser)
+print(Hello)
